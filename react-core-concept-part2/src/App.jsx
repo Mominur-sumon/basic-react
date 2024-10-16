7d651afe64676487570983d0d4d391880ab3c555
Md Mominur Rahman <mominur.sumon1@gmail.com>
2024-10-16 22:02:06 +0600

    button functionality of react

---
+++ react-core-concept-part2/src/App.jsx
@@ -0,0 +1,36 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  
+  function handleClick() {
+    alert('Button Clicked')
+  }
+
+  const handleClick2 = () => {
+    alert('Button Clicked 2')
+  }
+
+  const addToFive = (num) => {
+    alert(num + 5)
+  }
+
+  return (
+    <>
+      
+      <h3>React Core Concept 2</h3>
+      <button onClick={handleClick}>Click Me</button>
+      <button onClick={handleClick2}>Click Me 2</button>
+
+      {/* third way */}
+      <button onClick={() => alert('Button Clicked 3')}>Click Me 3</button>
+
+      <button onClick={() => addToFive(3)}>forth Button</button>
+    </>
+  )
+
+}
+
+export default App
