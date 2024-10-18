ba09f1b7573fad05ed8bb9cc44b0514b8e412bae
Md Mominur Rahman <mominur.sumon1@gmail.com>
2024-10-18 21:33:34 +0600

    useState of react added

--- react-conceptual-session/src/App.jsx
+++ react-conceptual-session/src/App.jsx
@@ -5,6 +5,25 @@ import './App.css'
 import Products from './components/Products/Products'
 
 function App() {
+  const [state, setState] = useState(0);
+
+
+  let storageVar = 0;
+  // console.log(state);
+
+  const handleState = () => {
+    setState(state + 1);
+  }
+
+  const handleVariable = () => {
+    storageVar = storageVar + 1;
+
+  }
+
+  const handleHeroAlam = (message) => {
+    console.log("message : ", message);
+  }
+
 
   const information = {
     message: "Secret Message",
@@ -15,7 +34,17 @@ function App() {
     <>
       <Products info={information}></Products>
       <h1>Vite + React</h1>
-      
+
+      <div>
+        <h1>This is State Count {state}</h1>
+        <h1>This is variable count {storageVar}</h1>
+      </div>
+
+      <button className='hero' onClick={handleState}>Change State Count </button>
+      <button onClick={handleVariable}>Change Variable Count </button>
+
+      <button onClick={() => handleHeroAlam("okay hero Alam")}>Handle Hero Alom</button>
+
     </>
   )
 }
