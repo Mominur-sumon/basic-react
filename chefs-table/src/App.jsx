3a376b45a8c53d5812c9886bdaadf7ed73d0a3b7
Md Mominur Rahman <mominur.sumon1@gmail.com>
2024-11-08 04:08:24 +0600

    added except calculation functionality

--- chefs-table/src/App.jsx
+++ chefs-table/src/App.jsx
@@ -10,45 +10,66 @@ import SideBar from "./SideBar";
 const App = () => {
 
   const [recipieQueue, setRecipieQueue] = useState([])
+  const [preparedRecipies, setPreparedRecipies] = useState([])
 
-  const addRecipeToQueue = (recipie) =>{
+  const addRecipeToQueue = (recipie) => {
     const isExist = recipieQueue.find(item => item.recipe_id === recipie.recipe_id)
     // console.log(recipie);
-    if(!isExist){
+    if (!isExist) {
       setRecipieQueue([...recipieQueue, recipie])
     } else {
       alert('Already added to queue')
     }
   }
 
-  console.log(recipieQueue);
+  const addPreparedRecipie = (id) => {
+    const deletedRecipie = recipieQueue.find(item => item.recipe_id === id)
+    const updatedQueue = recipieQueue.filter(item => item.recipe_id !== id)
+    setRecipieQueue(updatedQueue)
+    setPreparedRecipies([...preparedRecipies, deletedRecipie])
+
+  }
+  const [totalTime, setTotalTime] = useState(0)
+  const [TotalCalories, setTotalCalories] = useState(0)
+
+  const calculateTimeAndCaloeies = (time, cal) => {
+    setTotalTime(totalTime + Number(time))
+    setTotalCalories(TotalCalories + cal)
+  }
+
+  console.log(preparedRecipies);
+    return (
+      <div className="container mx-auto px-3">
+        {/* Header section */}
+        <Header></Header>
+        {/* Banner section */}
+        <Banner></Banner>
+
+        {/* Our recipies */}
+        <OurRecipies></OurRecipies>
+
+        {/* Recipie Cart Section */}
+        <section className="flex flex-col md:flex-row gap-6">
+
+          {/* Card section */}
+          <Recipies addRecipeToQueue={addRecipeToQueue}></Recipies>
+
+          {/* side bar */}
+          <SideBar 
+          recipieQueue={recipieQueue} 
+          addPreparedRecipie= {addPreparedRecipie} 
+          preparedRecipies= {preparedRecipies} 
+          calculateTimeAndCaloeies= {calculateTimeAndCaloeies}
+          totalTime={totalTime}
+          TotalCalories={TotalCalories}
+          ></SideBar>
+
+        </section>
 
 
-  return (
-    <div className="container mx-auto px-3">
-      {/* Header section */}
-      <Header></Header>
-      {/* Banner section */}
-      <Banner></Banner>
 
-      {/* Our recipies */}
-      <OurRecipies></OurRecipies>
+      </div>
+    );
+  };
 
-      {/* Recipie Cart Section */}
-      <section className="flex flex-col md:flex-row gap-6">
-
-        {/* Card section */}
-        <Recipies addRecipeToQueue={addRecipeToQueue}></Recipies>
-
-        {/* side bar */}
-        <SideBar recipieQueue= {recipieQueue}></SideBar>
-
-      </section>
-
-
-
-    </div>
-  );
-};
-
-export default App;
+  export default App;
--- chefs-table/src/SideBar.jsx
+++ chefs-table/src/SideBar.jsx
@@ -1,12 +1,20 @@
+/* eslint-disable react/prop-types */
 import PropTypes from "prop-types";
 
 
-const SideBar = ({ recipieQueue }) => {
+const SideBar = ({ recipieQueue, 
+    addPreparedRecipie, 
+    preparedRecipies, 
+    calculateTimeAndCaloeies, 
+    totalTime, TotalCalories }) => {
+    
     return (
+        
         <div className="md:w-1/3 border-2 rounded-2xl text-gray-600 p-2 bg-base-100">
             {/* Want to cook Table */}
-            <h2 className="border-b-2 py-2 mx-auto font-semibold text-2xl text-gray-800 text-center">Want To Cook : {recipieQueue.length} </h2>
+           
             <div className="overflow-x-auto">
+            <h2 className="border-b-2 py-2 mx-auto font-semibold text-2xl text-gray-800 text-center">Want To Cook : {recipieQueue.length} </h2>
                 <table className="table">
                     {/* head */}
                     <thead>
@@ -21,7 +29,7 @@ const SideBar = ({ recipieQueue }) => {
                     <tbody>
                         {/* row 1 */}
                         {
-                            recipieQueue.map((recipie,index) =>{
+                            recipieQueue.map((recipie, index) => {
                                 return (
                                     <tr className="hover" key={recipie.recipe_id}>
                                         <th>{index + 1}</th>
@@ -29,22 +37,61 @@ const SideBar = ({ recipieQueue }) => {
                                         <td>{recipie.preparing_time}</td>
                                         <td>{recipie.calories}</td>
                                         <td>
-                                            <button className="btn bg-green-400 text-gray-800 rounded-full px-4 font-medium">Preparing</button>
+                                            <button onClick={() => addPreparedRecipie(recipie.recipe_id)} onAuxClick={()=>calculateTimeAndCaloeies(recipie.preparing_time, recipie.calories)} className="btn bg-green-400 text-gray-800 rounded-full px-4 font-medium">Preparing</button>
                                         </td>
                                     </tr>
                                 )
                             })
                         }
-                      
+
                     </tbody>
                 </table>
             </div>
 
             {/* Cuurently cooking table */}
+            <div className="overflow-x-auto mt-8">
+                <h2 className="border-b-2 py-2 mx-auto font-semibold text-2xl text-gray-800 text-center">Current Cooking : {preparedRecipies.length} </h2>
+                <table className="table">
+                    {/* head */}
+                    <thead>
+                        <tr>
+                            <th></th>
+                            <th>Name</th>
+                            <th>Time</th>
+                            <th>Calories</th>
+
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {/* row 1 */}
+                        {
+                            preparedRecipies.map((prepare, index) => {
+                                return (
+                                    <tr className="hover" key={prepare.recipe_id}>
+                                        <th>{index + 1}</th>
+                                        <td>{prepare.recipe_name}</td>
+                                        <td>{prepare.preparing_time}</td>
+                                        <td>{prepare.calories}</td>
+
+                                    </tr>
+                                )
+                            })
+                        }
+                        <tr className="border-none">
+                            <th></th>
+                            <td></td>
+                            <td>Total Time = {totalTime} </td>
+                            <td>Total calories = {TotalCalories}</td>
+
+                        </tr>
+                    </tbody>
+                    
+                </table>
+            </div>
         </div>
     );
 };
 SideBar.PropTypes = {
-    recipieQueue: PropTypes.array.isRequired
+    recipieQueue: PropTypes.array.isRequired,
 }
 export default SideBar;
